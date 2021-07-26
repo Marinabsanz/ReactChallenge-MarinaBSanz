199e887b237c016ecdf42cae6dfca8695ffd5cc9
Marina Benitez Sanchez <marinabsanz@gmail.com>
2021-07-26 15:40:59 +0200

    CONFIG.JSON adding things

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
           Hello World . I'm Marina and I'm Starting a new project 
           </p>
         <a
-          className="App-link"
+         
           href="https://github.com/MarinaBsanz"
           target="_blank"
           rel="noopener noreferrer"
